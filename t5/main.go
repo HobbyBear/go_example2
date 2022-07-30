67aa074d1d446a04aa76e85baa3f11116be5c0a0
xiongch <xiongch@flatincbr.com>
2022-07-30 11:03:38 +0800

    code

--- t5/main.go
+++ t5/main.go
@@ -1,8 +1,8 @@
 package main
 
-func Add(a int32, b int32) int32 {
+func Add(a int32, b int32) (int32, bool) {
 	//fmt.Println(unsafe.Sizeof(a))
-	return a + b
+	return a + b, true
 }
 
 func main() {
